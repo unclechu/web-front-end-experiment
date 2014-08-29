48cc6aacd4e8bd6a00211723bd31ece7a6692a15
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-08-29 05:23:21 +0400

    progress

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,8 @@
+/**
+ * @author Viacheslav Lotsmanov
+ * @license GNU/GPLv3 by Free Software Foundation
+ */
+
 var gulp = require('gulp');
 var argv = require('yargs').argv;
 
@@ -5,24 +10,95 @@ var clean = require('gulp-clean');
 var taskListing = require('gulp-task-listing');
 var bower = require('gulp-bower');
 var symlink = require('gulp-symlink');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var gulpif = require('gulp-if');
+var minCSS = require('gulp-minify-css');
+var browserify = require('gulp-browserify');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
 gulp.task('help', taskListing);
 
 var production = argv.production ? true : false;
 
+// bower {{{1
+
 gulp.task('bower', function () {
-	return bower()
-		.pipe(gulp.dest('./js/lib/'));
+	return bower();
 });
 
-gulp.task('clean-bower', function () {
+// bower }}}1
+
+// css {{{1
+
+gulp.task('clean-css', function () {
 	return gulp
-		.src('./js/lib/')
+		.src('./css/build/')
 		.pipe(clean());
 });
 
+gulp.task('css', ['clean-css'], function () {
+	return gulp
+		.src('./css/src/main.scss')
+		.pipe(sass())
+		.pipe(gulpif(production, minCSS()))
+		.pipe(rename('build.css'))
+		.pipe(gulp.dest('./css/build/'));
+});
+
+// css }}}1
+
+// js {{{1
+
+gulp.task('clean-js', function () {
+	return gulp
+		.src('./js/build/')
+		.pipe(clean());
+});
+
+gulp.task('jshint', function () {
+	return gulp
+		.src('./js/src/**/*.js')
+		.pipe(jshint({
+			"browser": true,
+			"node": true,
+			"undef": true,
+			"indent": 1,
+			"sub": false,
+			"unused": true,
+			"predef": [ "define" ]
+		}))
+		.pipe(jshint.reporter(stylish));
+});
+
+gulp.task('js', ['clean-js', 'bower', 'jshint'], function () {
+	return gulp
+		.src('./js/src/main.js')
+		.pipe(browserify({
+			shim: {
+				jquery: {
+					path: './bower_components/jquery/dist/jquery',
+					exports: '',
+				},
+			},
+			debug: !production,
+		}))
+		.pipe(gulpif(production, uglify({
+			preserveComments: 'some',
+		})))
+		.pipe(rename('build.js'))
+		.pipe(gulp.dest('./js/build/'));
+});
+
+// js }}}1
+
+// clean {{{1
+
 gulp.task('clean', [
-	'clean-bower',
+	'clean-css',
+	'clean-js',
 ]);
 
 gulp.task('distclean', ['clean'], function () {
@@ -35,6 +111,10 @@ gulp.task('distclean', ['clean'], function () {
 		.pipe(clean());
 });
 
+// clean }}}1
+
+// deploy {{{1
+
 gulp.task('gulp-symlink', function () {
 	return gulp
 		.src('./node_modules/.bin/gulp')
@@ -43,7 +123,10 @@ gulp.task('gulp-symlink', function () {
 		}));
 });
 
+// deploy }}}1
+
 gulp.task('default', [
 	'gulp-symlink',
-	'bower',
+	'css',
+	'js',
 ]);
---
+++ js/src/main.js
@@ -0,0 +1,10 @@
+/**
+ * @author Viacheslav Lotsmanov
+ * @license GNU/GPLv3 by Free Software Foundation
+ */
+
+var $ = require('jquery');
+
+$(function () {
+	$('p').css('background', 'orange');
+});
